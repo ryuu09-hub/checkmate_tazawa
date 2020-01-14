9a09259183b224cd81b33354c379952fb080daa9
ryuu09-hub <wave9c@gmail.com>
2020-01-14 17:47:56 +0900

    data upload

--- data/camera.py
+++ data/camera.py
@@ -1,13 +1,13 @@
 import picamera
 from time import sleep
 
-photo_filename = '/data_pi'
+photo_filename = 'data_pi'
 
 # pi camera 用のライブラリーを使用して、画像を取得
 while True:
-    key = input("商品名選択,いちご「i」,オランジーナ「o」,ファンタ「f」,カルピス「k」,もも「m」,VitaminWater「v」")
-    drink ={"i":"ichigo","o":"orangena","f":"fantagrape","c":"calpis","m":"mono","v":"VitaminWater"}
-    for i in range(20):#20枚撮影
+    key = input("商品名選択,いちご「i」,オランジーナ「o」,ファンタ「f」,カルピス「k」,もも「m」,VitaminWater「v」,あやたか「a」")
+    drink ={"i":"ichigo","o":"orangena","f":"fantagrape","c":"calpis","m":"momo","v":"VitaminWater","a":"ayataka"}
+    for i in range(3):#20枚撮影
         with picamera.PiCamera() as camera:
             camera.resolution = (300,400)
             camera.start_preview()
@@ -15,6 +15,12 @@ while True:
             camera.capture("{}/{}/image_{}.jpg".format(photo_filename,drink[key],i))
             camera.stop_preview()
             
-            if key == 'o':
-                key = input("もう一つのサンプルを取得します。準備できたら「Enter」")
-                continue
+    if key == 'o':
+        key = input("もう一つのサンプルを取得します。準備できたら「Enter」")
+        for i in range(60):#20枚撮影
+            with picamera.PiCamera() as camera:
+                camera.resolution = (300,400)
+                camera.start_preview()
+                sleep(5)
+                camera.capture("{}/{}/image_{}.jpg".format(photo_filename,drink[key],i))
+                camera.stop_preview()
