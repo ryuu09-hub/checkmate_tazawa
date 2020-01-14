a4d8eb7e4aca045f482bcc7e0ef2ad540b7d65a4
ryuu09-hub <wave9c@gmail.com>
2020-01-14 15:46:35 +0900

    data upload

--- data/.ipynb_checkpoints/camera-checkpoint.py
+++ data/.ipynb_checkpoints/camera-checkpoint.py
@@ -7,15 +7,14 @@ photo_filename = '/data_pi'
 while True:
     key = input("商品名選択,いちご「i」,オランジーナ「o」,ファンタ「f」,カルピス「k」,もも「m」,VitaminWater「v」")
     drink ={"i":"ichigo","o":"orangena","f":"fantagrape","c":"calpis","m":"mono","v":"VitaminWater"}
-
-for i in range(20):#20枚撮影
-    with picamera.PiCamera() as camera:
-        camera.resolution = (300,400)
-        camera.start_preview()
-        sleep(5)
-        camera.capture("{}/{}/image_{}.jpg".format(photo_filename,drink[key],i))
-        camera.stop_preview()
-        
-        if key == 'o':
-            key = input("もう一つのサンプルを取得します。準備できたら「Enter」")
-            continue
+    for i in range(20):#20枚撮影
+        with picamera.PiCamera() as camera:
+            camera.resolution = (300,400)
+            camera.start_preview()
+            sleep(5)
+            camera.capture("{}/{}/image_{}.jpg".format(photo_filename,drink[key],i))
+            camera.stop_preview()
+            
+            if key == 'o':
+                key = input("もう一つのサンプルを取得します。準備できたら「Enter」")
+                continue
